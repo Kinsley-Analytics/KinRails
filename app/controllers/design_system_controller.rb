58d4e01746412dfd0c36077cfdc3e20232e3f87c
Casey Robinson <casey.robinson@firespring.com>
2026-03-11 12:00:25 +0800

    update to base design system controller

--- app/controllers/design_system_controller.rb
+++ app/controllers/design_system_controller.rb
@@ -1,4 +1,9 @@
 class DesignSystemController < ApplicationController
+  skip_after_action :verify_authorized
+  skip_after_action :verify_policy_scoped
+
+  layout "design_system"
+
   def show
   end
 end
